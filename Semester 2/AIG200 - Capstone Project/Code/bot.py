ba4897f3bcbfd24141e36b1bb91709a300336a23
jcp-tech <jonathan.chacko@maritimesupports.com>
2025-05-24 19:06:17 -0400

    Updated

--- Semester 2/AIG200 - Capstone Project/Code/bot.py
+++ Semester 2/AIG200 - Capstone Project/Code/bot.py
@@ -4,7 +4,7 @@ pip install -U discord.py
 pip install python-dotenv
 """
 
-import os
+import os, uuid
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
@@ -13,7 +13,10 @@ load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
-intents.message_content = True  # Needed for reading messages
+intents.message_content = True
+intents.messages = True
+intents.guilds = True
+intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
@@ -24,6 +27,29 @@ async def on_ready():
 @bot.command()
 async def hello(ctx):
     await ctx.send("Hello there!")
+
+### <Message id=1375158437746442272 channel=<Thread id=1375152189630185513 name='@Spendify Practice Thread!' parent=aig-200-capstone owner_id=686190428634349751 locked=False archived=False> type=<MessageType.default: 0> author=<Member id=686190428634349751 name='jcp99gamer' global_name='Jonathan Chacko' bot=False nick='Dead Ketchup' guild=<Guild id=730471027905134712 name='Maple Masala' shard_id=0 chunked=True member_count=4>> flags=<MessageFlags value=0>>
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    # If message has attachments (e.g. image)
+    if message.attachments:
+        for attachment in message.attachments:
+            if attachment.content_type.startswith("image/"):
+                session_id = str(uuid.uuid4())
+                file_extension = attachment.filename.split('.')[-1]
+                file_name = f"{session_id}.{file_extension}"
+                file_path = os.path.join(os.getcwd(), file_name)
+
+                await attachment.save(file_path)
+
+                print(f"[📸] Image saved as: {file_name}")
+                print(f"[👤] Uploaded by: {message.author.name}")
+                # print(message)
+
+    await bot.process_commands(message)
 
 # Run the bot (replace with your token)
 bot.run(token)
