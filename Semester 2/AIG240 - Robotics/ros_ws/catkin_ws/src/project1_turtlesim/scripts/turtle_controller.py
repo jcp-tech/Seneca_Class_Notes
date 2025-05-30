5be7a45eff13a2fdcfabb9cd29504e3c35aa2659
Jonathan Chacko <jetauto@seneca-jcp-tech-ubuntu.mshome.net>
2025-05-29 20:32:53 -0400

    ROS Project 1 Updated

--- Semester 2/AIG240 - Robotics/ros_ws/catkin_ws/src/project1_turtlesim/scripts/turtle_controller.py
+++ Semester 2/AIG240 - Robotics/ros_ws/catkin_ws/src/project1_turtlesim/scripts/turtle_controller.py
@@ -2,73 +2,63 @@
 # -*- coding: utf-8 -*-
 
 import rospy
+import sys, select, termios, tty
 from geometry_msgs.msg import Twist
-from pynput import keyboard
 
-pressed_keys = set()
-listener = None  # So we can stop it later
+settings = termios.tcgetattr(sys.stdin)
 
-def calculate_twist():
-    twist = Twist()
-    if 'w' in pressed_keys:
-        twist.linear.x = 2.0
-    elif 's' in pressed_keys:
-        twist.linear.x = -2.0
-
-    if 'a' in pressed_keys:
-        twist.angular.z = 2.0
-    elif 'd' in pressed_keys:
-        twist.angular.z = -2.0
-
-    return twist
-
-def on_press(key):
-    try:
-        k = key.char.lower()
-        if k in ['w', 'a', 's', 'd']:
-            pressed_keys.add(k)
-    except AttributeError:
-        pass
-
-def on_release(key):
-    try:
-        k = key.char.lower()
-        if k in pressed_keys:
-            pressed_keys.remove(k)
-    except AttributeError:
-        pass
+def get_key():
+    tty.setraw(sys.stdin.fileno())
+    select_ready, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if select_ready:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
 
 def main():
-    import sys
-    global listener
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    rospy.init_node('turtle_keyboard_controller')
+    move_cmd = Twist()
 
-    if len(sys.argv) < 2:
-        print("Usage: rosrun project1_turtlesim turtle_controller.py <turtle_name>")
-        return
+    instructions = """
+Control the turtle with:
+  w: forward
+  s: backward
+  a: turn left
+  d: turn right
+  x: stop
+  q: quit
+"""
+    print(instructions)
 
-    turtle_name = sys.argv[1]
-    topic = '/' + turtle_name + '/cmd_vel'
+    while not rospy.is_shutdown():
+        key = get_key()
+        if key == 'w':
+            move_cmd.linear.x = 2.0
+            move_cmd.angular.z = 0.0
+        elif key == 's':
+            move_cmd.linear.x = -2.0
+            move_cmd.angular.z = 0.0
+        elif key == 'a':
+            move_cmd.linear.x = 0.0
+            move_cmd.angular.z = 2.0
+        elif key == 'd':
+            move_cmd.linear.x = 0.0
+            move_cmd.angular.z = -2.0
+        elif key == 'x':
+            move_cmd.linear.x = 0.0
+            move_cmd.angular.z = 0.0
+        elif key == 'q':
+            break
+        else:
+            continue
 
-    rospy.init_node('turtle_controller_v2', anonymous=True)
-    pub = rospy.Publisher(topic, Twist, queue_size=10)
-    rate = rospy.Rate(10)
-
-    print("Multi-key controller active. Use W/A/S/D. Ctrl+C to quit.")
-
-    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-    listener.start()
-
-    try:
-        while not rospy.is_shutdown():
-            twist = calculate_twist()
-            pub.publish(twist)
-            rate.sleep()
-    except rospy.ROSInterruptException:
-        pass
-    finally:
-        print("\nShutting down controller...")
-        if listener:
-            listener.stop()
+        pub.publish(move_cmd)
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
