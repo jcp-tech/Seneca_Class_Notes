a980885598e4e3953de9d53d0219ccddba0d66fb
jcp-tech <jonathan.chacko@maritimesupports.com>
2025-06-03 16:30:53 -0400

    Assignment 2 - CV

--- Semester 2/AIG210 - Computer Vision/Assignment 2 - Group 5/Assignment2_2.py
+++ Semester 2/AIG210 - Computer Vision/Assignment 2 - Group 5/Assignment2_2.py
@@ -1,42 +1,56 @@
-import numpy as np
-import cv2 as cv
+# import numpy as np
+# import cv2
 
-drawing = False # true if mouse is pressed
-mode = True # if True, draw rectangle. Press 'm' to toggle to curve
-ix,iy = -1,-1
+# drawing = False  # (Used for drawing state)
+# mode = True  # PART 3a: True = polylines, False = polygon
+# points = []
 
-# mouse callback function
-def draw_circle(event,x,y,flags,param):
-    global ix,iy,drawing,mode
+# # PART 3a: Black image used
+# img = np.zeros((512, 512, 3), np.uint8)
+# img_copy = img.copy()
 
-    if event == cv.EVENT_LBUTTONDOWN:
-        drawing = True
-        ix,iy = x,y
+# polyline_colour = (0, 255, 0)
+# polygon_colour = (255, 0, 255)
 
-    elif event == cv.EVENT_MOUSEMOVE:
-        if drawing == True:
-            if mode == True:
-                cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-            else:
-                cv.circle(img,(x,y),5,(0,0,255),-1)
+# def draw_circle(event, x, y, flags, param):
+#     global drawing, points, mode, img
 
-    elif event == cv.EVENT_LBUTTONUP:
-        drawing = False
-        if mode == True:
-            cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-        else:
-            cv.circle(img,(x,y),5,(0,0,255),-1)
+#     if event == cv2.EVENT_LBUTTONDOWN:
+#         drawing = True
+#         points.append((x, y))
 
-img = np.zeros((512,512,3), np.uint8)
-cv.namedWindow('image')
-cv.setMouseCallback('image',draw_circle)
- 
-while(1):
-    cv.imshow('image',img)
-    k = cv.waitKey(1) & 0xFF
-    if k == ord('m'):
-        mode = not mode
-    elif k == 27:
-        break
- 
-cv.destroyAllWindows()
+#     elif event == cv2.EVENT_RBUTTONDOWN:
+#         drawing = False
+#         if len(points) > 1:
+#             pts = np.array(points, np.int32)
+#             pts = pts.reshape((-1, 1, 2))
+
+#             # PART 3a + PART 2: Drawing polyline with changed thickness
+#             if mode:
+#                 cv2.polylines(img, [pts], isClosed=False, color=polyline_colour, thickness=2)
+#             else:
+#                 cv2.polylines(img, [pts], isClosed=True, color=polygon_colour, thickness=2)
+#         points = []
+
+# cv2.namedWindow('image')
+# cv2.setMouseCallback('image', draw_circle)
+
+# while True:
+#     temp_img = img.copy()
+
+#     if drawing and len(points) > 1:
+#         for i in range(len(points) - 1):
+#             cv2.line(temp_img, points[i], points[i + 1], (200, 200, 200), 1)
+
+#     cv2.imshow('image', temp_img)
+#     k = cv2.waitKey(100) & 0xFF # PART 2: Lowered refresh rate (was 1)
+
+#     if k == ord('m'):
+#         mode = not mode  # PART 3a: Toggle between polyline and polygon
+#     elif k == ord('x'):
+#         cv2.imwrite('saved_image.png', img)  # PART 3b: Save image
+#         print("Image saved as 'saved_image.png'")
+#     elif k == 27:  # ESC to exit
+#         break
+
+# cv2.destroyAllWindows()
